48e598632ab59470c594c1ea9e1355af9b4562c8
Marcos Moraes <contato@marcosamoraes.com>
2023-01-19 18:40:09 -0300

    routes defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './globalStyles.js';
+import Login from './screens/Login/Login.js';
+import Register from './screens/Register/Register.js';
+import Home from './screens/Home/Home.js';
+import ValuesEntry from './screens/ValuesEntry/ValuesEntry.js';
+import ValuesOutput from './screens/ValuesOutput/ValuesOutput.js';
 
 export default function App () {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-    </>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/nova-entrada" element={<ValuesEntry />} />
+        <Route path="/nova-saida" element={<ValuesOutput />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
